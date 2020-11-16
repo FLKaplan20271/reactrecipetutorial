ff481cbc14745c0296d9019b1c1ac46f04a17c83
FLKaplan20271 <frank.kaplan@gmail.com>
2020-11-16 00:41:33 -0700

    broken app again - error React Hook useEffect has a missing dependency

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Recipe = () => { 
+const Recipe = ({title, calories, cautions,image, ingredients}) => { 
     return(
         <div>
-            <h1>Title</h1>
-            <p>Description</p>
-            <img src="" alt=""/>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <p>{cautions}</p>
+            <img src={image} alt=""/>
         </div>
         );
 }
